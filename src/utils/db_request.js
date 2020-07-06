e655f1fa7c3657cd6d5d7c3557373d1cbaf306c4
phuhung273 <phuhung.tranpham@gmail.com>
2020-07-06 21:30:35 +0700

    initial

---
+++ src/utils/db_request.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BACKEND_BASE_URL, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+export default service
